8f6e02bd825fbab3bb42eb61fd4b71377c8a497f
Jeremy Mung <miroslav1990@gmail.com>
2015-01-09 00:38:01 +0800

    comment unreachable links visiting from China.
    All services provided by Google have been blocked, so it had better to
    comment these links to improve loading speed.

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 <script type="text/javascript">
 $("#blog-loading div").animate({width:"65%"})
 </script>
-    
+<!--    
 	<?php if (is_home()) : ?>
 	<div class="sidebar-box clearfix topcomment">
 		<h4>本周口水榜</h4>
@@ -17,13 +17,14 @@ $("#blog-loading div").animate({width:"65%"})
 		<?php include('includes/recentcomment.php'); ?>
 	</div>
 	<?php endif; ?>        
-
+-->
             <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar1') ) : ?>
             <?php endif; ?>
             <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar2') ) : ?>        
             <?php endif; ?>
     
     <!-- 下面这段AD代码希望亲爱的朋友们给予保留，这是本免费主题唯一的要求了，谢谢！ -->
+<!--
     <?php if (is_home()) : ?>
     <div class="sidebar-box clearfix">
 		<h4>赞助推荐</h4>
@@ -31,7 +32,7 @@ $("#blog-loading div").animate({width:"65%"})
         <a href="http://www.chinablogs.org" title="中国博客联盟 独立博客交流平台" target="_blank"><img src="http://www.chinablogs.org/images/cbs-250x60.png"></a>
 	</div>
     <?php endif; ?>
-    
+-->    
 </div>
 <!-- END sidebar -->
 
