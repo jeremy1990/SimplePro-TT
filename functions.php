9c34123e580e76bd1d85bfcceded5e2ee2df7621
Jeremy Mung <miroslav1990@gmail.com>
2015-11-01 16:03:48 +0800

    Add more editor buttons

--- functions.php
+++ functions.php
@@ -825,4 +825,28 @@ return '<div class="download"><a href="'.$href.'">'.$content.'</a></div>';
 
 add_shortcode("dl", "download");
 
+function add_editor_buttons($buttons) {
+    $buttons[] = 'fontselect';
+    $buttons[] = 'fontsizeselect';
+    $buttons[] = 'cleanup';
+    $buttons[] = 'styleselect';
+    $buttons[] = 'hr';
+    $buttons[] = 'del';
+    $buttons[] = 'sub';
+    $buttons[] = 'sup';
+    $buttons[] = 'copy';
+    $buttons[] = 'paste';
+    $buttons[] = 'cut';
+    $buttons[] = 'undo';
+    $buttons[] = 'image';
+    $buttons[] = 'anchor';
+    $buttons[] = 'backcolor';
+    $buttons[] = 'wp_page';
+    $buttons[] = 'charmap';
+    $buttons[] = 'code';
+
+    return $buttons;
+}
+add_filter("mce_buttons_3", "add_editor_buttons");
+
 ?>
